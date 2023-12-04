398a2911756b53b0fdec0ce9d5cf880cf24cd67c
GoldBomborka <124084574+GoldBomborka@users.noreply.github.com>
2023-12-04 14:07:19 +0100

    Update myspider.py

--- eventbrite/eventbrite/spiders/myspider.py
+++ eventbrite/eventbrite/spiders/myspider.py
@@ -8,7 +8,7 @@ class MySpider(scrapy.Spider):
         'FEEDS': {
             'output.json': {
                 'format': 'json',
-                'overwrite': True,    # If the file already exists, it will overwrite it
+                'overwrite': True,  # If the file already exists, it will overwrite it
             },
         },
     }
